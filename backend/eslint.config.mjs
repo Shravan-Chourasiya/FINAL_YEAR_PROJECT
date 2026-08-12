e2cae238dc1423207943b4df304146bbacafe97a
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-08-12 22:38:13 +0530

    chore: add .env.example for environment configuration and update ESLint config for project service

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -15,7 +15,9 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["*.config.ts"],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
