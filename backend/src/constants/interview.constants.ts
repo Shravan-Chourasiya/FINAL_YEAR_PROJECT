6cb7eada3d1fd1b609d6ef3dba58248883596c5f
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-08-26 22:01:07 +0530

    feat(constants): add TARGET_COMPANIES constant with a list of major tech companies

---
+++ backend/src/constants/interview.constants.ts
@@ -0,0 +1,17 @@
+export const TARGET_COMPANIES=[
+    "Google",
+    "Microsoft",
+    "Amazon",
+    "Meta",
+    "Apple",
+    "Netflix",
+    "OpenAI",
+    "Nvidia",
+    "TCS",
+    "Infosys",
+    "JPMorgan",
+    "Wipro",
+    "Deloitte",
+    "Adobe",
+    "Anthropic"
+]
